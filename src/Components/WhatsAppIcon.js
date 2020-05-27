00c2928b48b255902aff1a2556c53af367d09d8a
Ratti Saez <pablo.e.ratti.saez@accenture.com>
2020-05-27 00:17:14 -0300

    Whatsapp fixed in mobile

--- src/Components/WhatsAppIcon.js
+++ src/Components/WhatsAppIcon.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { isMobile } from 'react-device-detect';
 export default class WhatsAppIcon extends Component {
     constructor(props) {
         super(props);
         this.state = {
             W_input: 'En que podemos ayudarle?',
-            modalOpen: true,
+            modalOpen: false,
             
         }
         this.changeHandler = this.changeHandler.bind(this);
@@ -29,23 +30,33 @@ export default class WhatsAppIcon extends Component {
         console.log("Submited");        
         let msg = this.state.W_input;
         let wspLink = 'https://api.whatsapp.com/send?phone=2235911061&text='+msg;
-        window.location = wspLink ;
+        //Agregar validacion para mobile
+        window.location = wspLink;
         
+    }
+
+    handleSubmitMobile(e){
+       
+        console.log("Submited Mobile");    
+        let wspLink = 'https://api.whatsapp.com/send?phone=2235911061';
+        window.location = wspLink;
     }
     render() {
         let mustOpen = this.state.modalOpen;
+      
         return (
             <WhatsAppContainer  >
-                
+                {isMobile ? <img onClick={() => this.handleSubmitMobile()} className="icon" src="img/whatsApp.png" /> :
                 <img onClick={() => this.openModal()} className="icon" src="img/whatsApp.png" />
-             
+                }
                 {mustOpen ? 
                     <form onSubmit={this.handleSubmit} id="whatsapp-txt">
 
                         <input onChange={this.changeHandler} className="w-input" type="text" placeholder="En que podemos ayudarle" />
                         <input className="mySubmit" type="submit" />
                     </form> : null
-                }
+                } 
+                
             </WhatsAppContainer>
         );
     }
@@ -54,7 +65,7 @@ export default class WhatsAppIcon extends Component {
 const WhatsAppContainer = styled.div` 
 
 
-@media screen and (max-width: 600px) {
+@media screen and (max-width: 300px) {
 #whatsapp-txt{
 display:none;
 }
